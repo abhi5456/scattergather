dc8e176ba0397860a4ea6758b00c0623099957ae
Minli Liao <19294238+petite2@users.noreply.github.com>
2020-11-26 15:45:27 -0500

    Initial commit

---
+++ sg_cache.c
@@ -0,0 +1,83 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  File           : sg_driver.c
+//  Description    : This file contains the driver code to be developed by
+//                   the students of the 311 class.  See assignment details
+//                   for additional information.
+//
+//   Author        : YOUR NAME
+//   Last Modified : 
+//
+
+// Include Files
+#include <stdlib.h>
+#include <cmpsc311_log.h>
+
+// Project Includes
+#include <sg_cache.h>
+
+// Defines
+
+// Functional Prototypes
+
+//
+// Functions
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : initSGCache
+// Description  : Initialize the cache of block elements
+//
+// Inputs       : maxElements - maximum number of elements allowed
+// Outputs      : 0 if successful, -1 if failure
+
+int initSGCache( uint16_t maxElements ) {
+
+    // Return successfully
+    return( 0 );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : closeSGCache
+// Description  : Close the cache of block elements, clean up remaining data
+//
+// Inputs       : none
+// Outputs      : 0 if successful, -1 if failure
+
+int closeSGCache( void ) {
+
+    // Return successfully
+    return( 0 );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : getSGDataBlock
+// Description  : Get the data block from the block cache
+//
+// Inputs       : nde - node ID to find
+//                blk - block ID to find
+// Outputs      : pointer to block or NULL if not found
+
+char * getSGDataBlock( SG_Node_ID nde, SG_Block_ID blk ) {
+
+    // Return successfully
+    return( 0 );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : putSGDataBlock
+// Description  : Get the data block from the block cache
+//
+// Inputs       : nde - node ID to find
+//                blk - block ID to find
+//                block - block to insert into cache
+// Outputs      : 0 if successful, -1 if failure
+
+int putSGDataBlock( SG_Node_ID nde, SG_Block_ID blk, char *block ) {
+
+    // Return successfully
+    return( 0 );
+}
---
+++ sg_driver.c
@@ -0,0 +1,260 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  File           : sg_driver.c
+//  Description    : This file contains the driver code to be developed by
+//                   the students of the 311 class.  See assignment details
+//                   for additional information.
+//
+//   Author        : 
+//   Last Modified : 
+//
+
+// Include Files
+
+// Project Includes
+#include <sg_driver.h>
+#include <sg_service.h>
+
+// Defines
+
+//
+// Global Data
+
+// Driver file entry
+
+// Global data
+int sgDriverInitialized = 0; // The flag indicating the driver initialized
+SG_Block_ID sgLocalNodeId;   // The local node identifier
+SG_SeqNum sgLocalSeqno;      // The local sequence number
+
+// Driver support functions
+int sgInitEndpoint( void ); // Initialize the endpoint
+
+//
+// Functions
+
+//
+// File system interface implementation
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : sgopen
+// Description  : Open the file for for reading and writing
+//
+// Inputs       : path - the path/filename of the file to be read
+// Outputs      : file handle if successful test, -1 if failure
+
+SgFHandle sgopen(const char *path) {
+
+    // First check to see if we have been initialized
+    if (!sgDriverInitialized) {
+
+        // Call the endpoint initialization 
+        if ( sgInitEndpoint() ) {
+            logMessage( LOG_ERROR_LEVEL, "sgopen: Scatter/Gather endpoint initialization failed." );
+            return( -1 );
+        }
+
+        // Set to initialized
+        sgDriverInitialized = 1;
+    }
+
+// FILL IN THE REST OF THE CODE
+
+    // Return the file handle 
+    return( 0 );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : sgread
+// Description  : Read data from the file
+//
+// Inputs       : fh - file handle for the file to read from
+//                buf - place to put the data
+//                len - the length of the read
+// Outputs      : number of bytes read, -1 if failure
+
+int sgread(SgFHandle fh, char *buf, size_t len) {
+
+
+    // Return the bytes processed
+    return( len );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : sgwrite
+// Description  : write data to the file
+//
+// Inputs       : fh - file handle for the file to write to
+//                buf - pointer to data to write
+//                len - the length of the write
+// Outputs      : number of bytes written if successful test, -1 if failure
+
+int sgwrite(SgFHandle fh, char *buf, size_t len) {
+
+    // Log the write, return bytes written
+    return( len );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : sgseek
+// Description  : Seek to a specific place in the file
+//
+// Inputs       : fh - the file handle of the file to seek in
+//                off - offset within the file to seek to
+// Outputs      : new position if successful, -1 if failure
+
+int sgseek(SgFHandle fh, size_t off) {
+
+    // Return new position
+    return( off );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : sgclose
+// Description  : Close the file
+//
+// Inputs       : fh - the file handle of the file to close
+// Outputs      : 0 if successful test, -1 if failure
+
+int sgclose(SgFHandle fh) {
+
+    // Return successfully
+    return( 0 );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : sgshutdown
+// Description  : Shut down the filesystem
+//
+// Inputs       : none
+// Outputs      : 0 if successful test, -1 if failure
+
+int sgshutdown(void) {
+
+    // Log, return successfully
+    logMessage( LOG_INFO_LEVEL, "Shut down Scatter/Gather driver." );
+    return( 0 );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : serialize_sg_packet
+// Description  : Serialize a ScatterGather packet (create packet)
+//
+// Inputs       : loc - the local node identifier
+//                rem - the remote node identifier
+//                blk - the block identifier
+//                op - the operation performed/to be performed on block
+//                sseq - the sender sequence number
+//                rseq - the receiver sequence number
+//                data - the data block (of size SG_BLOCK_SIZE) or NULL
+//                packet - the buffer to place the data
+//                plen - the packet length (int bytes)
+// Outputs      : 0 if successfully created, -1 if failure
+
+SG_Packet_Status serialize_sg_packet(SG_Node_ID loc, SG_Node_ID rem, SG_Block_ID blk,
+                                     SG_System_OP op, SG_SeqNum sseq, SG_SeqNum rseq, char *data,
+                                     char *packet, size_t *plen) {
+
+// REPLACE THIS CODE WITH YOU ASSIGNMENT #2
+
+    // Return the system function return value
+    return( 0 );
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : deserialize_sg_packet
+// Description  : De-serialize a ScatterGather packet (unpack packet)
+//
+// Inputs       : loc - the local node identifier
+//                rem - the remote node identifier
+//                blk - the block identifier
+//                op - the operation performed/to be performed on block
+//                sseq - the sender sequence number
+//                rseq - the receiver sequence number
+//                data - the data block (of size SG_BLOCK_SIZE) or NULL
+//                packet - the buffer to place the data
+//                plen - the packet length (int bytes)
+// Outputs      : 0 if successfully created, -1 if failure
+
+SG_Packet_Status deserialize_sg_packet(SG_Node_ID *loc, SG_Node_ID *rem, SG_Block_ID *blk,
+                                       SG_System_OP *op, SG_SeqNum *sseq, SG_SeqNum *rseq, char *data,
+                                       char *packet, size_t plen) {
+
+// REPLACE THIS CODE WITH YOU ASSIGNMENT #2
+
+    // Return the system function return value
+    return( 0 );
+}
+
+//
+// Driver support functions
+
+////////////////////////////////////////////////////////////////////////////////
+//
+// Function     : sgInitEndpoint
+// Description  : Initialize the endpoint
+//
+// Inputs       : none
+// Outputs      : 0 if successfull, -1 if failure
+
+int sgInitEndpoint( void ) {
+
+    // Local variables
+    char initPacket[SG_BASE_PACKET_SIZE], recvPacket[SG_BASE_PACKET_SIZE];
+    size_t pktlen, rpktlen;
+    SG_Node_ID loc, rem;
+    SG_Block_ID blkid;
+    SG_SeqNum sloc, srem;
+    SG_System_OP op;
+    SG_Packet_Status ret;
+
+    // Local and do some initial setup
+    logMessage( LOG_INFO_LEVEL, "Initializing local endpoint ..." );
+    sgLocalSeqno = SG_INITIAL_SEQNO;
+
+    // Setup the packet
+    pktlen = SG_BASE_PACKET_SIZE;
+    if ( (ret = serialize_sg_packet( SG_NODE_UNKNOWN, // Local ID
+                                    SG_NODE_UNKNOWN,   // Remote ID
+                                    SG_BLOCK_UNKNOWN,  // Block ID
+                                    SG_INIT_ENDPOINT,  // Operation
+                                    sgLocalSeqno++,    // Sender sequence number
+                                    SG_SEQNO_UNKNOWN,  // Receiver sequence number
+                                    NULL, initPacket, &pktlen)) != SG_PACKT_OK ) {
+        logMessage( LOG_ERROR_LEVEL, "sgInitEndpoint: failed serialization of packet [%d].", ret );
+        return( -1 );
+    }
+
+    // Send the packet
+    rpktlen = SG_BASE_PACKET_SIZE;
+    if ( sgServicePost(initPacket, &pktlen, recvPacket, &rpktlen) ) {
+        logMessage( LOG_ERROR_LEVEL, "sgInitEndpoint: failed packet post" );
+        return( -1 );
+    }
+
+    // Unpack the recieived data
+    if ( (ret = deserialize_sg_packet(&loc, &rem, &blkid, &op, &sloc, 
+                                    &srem, NULL, recvPacket, rpktlen)) != SG_PACKT_OK ) {
+        logMessage( LOG_ERROR_LEVEL, "sgInitEndpoint: failed deserialization of packet [%d]", ret );
+        return( -1 );
+    }
+
+    // Sanity check the return value
+    if ( loc == SG_NODE_UNKNOWN ) {
+        logMessage( LOG_ERROR_LEVEL, "sgInitEndpoint: bad local ID returned [%ul]", loc );
+        return( -1 );
+    }
+
+    // Set the local node ID, log and return successfully
+    sgLocalNodeId = loc;
+    logMessage( LOG_INFO_LEVEL, "Completed initialization of node (local node ID %lu", sgLocalNodeId );
+    return( 0 );
+}
