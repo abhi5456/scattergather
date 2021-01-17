7b7e4c295e7cc9ffd37ca425ba6637d8bf44357a
abhi5456 <abhimamidipaka@gmail.com>
2021-01-16 19:47:01 -0800

    final

--- sg_service.h
+++ sg_service.h
@@ -1,16 +1,6 @@
 #ifndef SG_SERVICE_INCLUDED
 #define SG_SERVICE_INCLUDED
 
-////////////////////////////////////////////////////////////////////////////////
-//
-//  File           : sg_service.h
-//  Description    : This is the declaration of the interface to the 
-//                   base functions for the ScatterGather service.
-//
-//   Author        : Patrick McDaniel
-//   Last Modified : Thu 03 Sep 2020 06:23:05 PM EDT
-//
-
 // Includes
 #include <sg_defs.h>
 
